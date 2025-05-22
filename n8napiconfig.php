8f0f454a80221554e229ec79657bad70b7439d0a
visualcd <87854032+visualcd@users.noreply.github.com>
2025-05-22 15:41:30 +0300

    Create n8napiconfig.php

---
+++ n8napiconfig.php
@@ -0,0 +1,10 @@
+<?php
+// config.php - Asigură-te că acest fișier nu este accesibil public
+return [
+    'DB_HOST'     => 'localhost',
+    'DB_USER'     => 'utilizaotr_baza_de_date',
+    'DB_PASSWORD' => 'parola_baza_de_date',
+    'DB_NAME'     => 'nume_baza_de_date',
+    'API_KEY'     => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+];
+?>
